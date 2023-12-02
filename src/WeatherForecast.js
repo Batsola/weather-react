45c5873e1058464d15b6f1fbd378bb8b8a00c4da
Batsola <drawstsu@gmail.com>
2023-12-02 13:34:09 +0300

    refactored code

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,7 +16,14 @@ export default function WeatherForecast(props) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
+  function load() {
+    let apiKey = `bd5b4461863eddaa6ced0a0a67989e0a`;
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
+    axios.get(apiUrl).then(handleResponse);
+  }
   if (loaded) {
     return (
       <div className="WeatherForecast">
@@ -36,13 +43,7 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiKey = `bd5b4461863eddaa6ced0a0a67989e0a`;
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
-
+    loaded();
     return null;
   }
 }
